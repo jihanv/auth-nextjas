6c55ad9d80ef4f3bd4cf0d7b352e4ceabe6c1880
jihanv <hedjmny@gmail.com>
2025-06-15 11:59:28 +0900

    Add comments to dbConfig

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,14 +1,17 @@
-export default async function UserProfile({ params }: { params: { id: string } }) {
+export default async function UserProfile({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
 
-    const {id} = await params
-  
-    return (
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl">
         Profile Page
-        <span className="p-2 rounded ml-2 bg-orange-500 text-white" >{id}</span>
+        <span className="p-2 rounded ml-2 bg-orange-500 text-white">{id}</span>
       </p>
     </div>
   );
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,19 +1,29 @@
+// Import the mongoose library for MongoDB object modeling
 import mongoose from "mongoose";
 
+// Define an asynchronous function to connect to MongoDB
 export async function connect() {
   try {
+    // Attempt to connect to the MongoDB database using the URI from environment variables
+    // The '!' after process.env.MONGO_URI tells TypeScript to assume this value is not undefined
+    // This skips safety checks and can cause a runtime error if the variable is actually missing
     mongoose.connect(process.env.MONGO_URI!);
+
+
+    // Access the default mongoose connection instance
     const connection = mongoose.connection;
 
-
+    // Event listener for a successful connection
     connection.on("connected", () => {
-        console.log("MongoDB Connected successfully.")
+      console.log("MongoDB Connected successfully.")
     })
 
+    // Event listener for connection errors
     connection.on("error", (err) => {
-        console.log("MongoDB connection error. please make sure MongoDB is running.", err)
+      console.log("MongoDB connection error. please make sure MongoDB is running.", err)
     })
   } catch (error) {
+    // Catch and log any error that occurs during the connection process
     console.log("Something went wrong!\n", error);
   }
 }
